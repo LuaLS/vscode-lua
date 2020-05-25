462f017a2ab8408d1723283ee2dff7b34eb4af4b
最萌小汐 <sumneko@hotmail.com>
2020-05-25 16:29:18 +0800

    生成 setting 的 schema

---
+++ setting.lua
@@ -0,0 +1,15 @@
+local fs = require 'bee.filesystem'
+
+require 'utility'
+local json = require 'json'
+local configuration = require 'package.configuration'
+
+local setting = {
+    ['$schema'] = '',
+    title       = 'setting',
+    description = 'Setting of sumneko.lua',
+    type        = 'object',
+    properties  = configuration,
+}
+
+io.save(fs.path'setting.json', json.encode(setting))
