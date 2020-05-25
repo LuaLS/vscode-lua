b106a0e21878bac1a813ff6c87b22ff2bd5d4885
最萌小汐 <sumneko@hotmail.com>
2020-05-25 16:44:53 +0800

    更新 schema

--- setting/build.lua
+++ setting/build.lua
@@ -4,12 +4,32 @@ require 'utility'
 local json = require 'json'
 local configuration = require 'package.configuration'
 
-local setting = {
-    ['$schema'] = '',
-    title       = 'setting',
-    description = 'Setting of sumneko.lua',
-    type        = 'object',
-    properties  = configuration,
-}
+local function copyWithNLS(t, callback)
+    local nt = {}
+    for k, v in pairs(t) do
+        if type(v) == 'string' then
+            nt[k] = callback(v) or v
+        elseif type(v) == 'table' then
+            nt[k] = copyWithNLS(v, callback)
+        else
+            nt[k] = v
+        end
+    end
+    return nt
+end
 
-io.save(fs.path'setting/schema.json', json.encode(setting))
+for _, lang in ipairs {'', '-zh-cn'} do
+    local nls = require('package.nls' .. lang)
+
+    local setting = {
+        ['$schema'] = '',
+        title       = 'setting',
+        description = 'Setting of sumneko.lua',
+        type        = 'object',
+        properties  = copyWithNLS(configuration, function (str)
+            return str:gsub('^%%(.+)%%$', nls)
+        end),
+    }
+
+    io.save(fs.path'setting/schema'..lang..'.json', json.encode(setting))
+end
