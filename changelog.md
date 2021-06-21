a38fb1782b82c888240692eb0a51abb42c772185
最萌小汐 <sumneko@hotmail.com>
2021-06-22 01:52:58 +0800

    2.0.1

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # changelog
 
+## 2.0.1
+`2021-6-21`
+* `FIX` [#566](https://github.com/sumneko/lua-language-server/issues/566)
+
 ## 2.0.0
 `2021-6-21`
 * `NEW` implement
--- package/build.lua
+++ package/build.lua
@@ -1,6 +1,6 @@
 local json = require 'json-beautify'
 
-local VERSION = "2.0.0"
+local VERSION = "2.0.1"
 
 local package = require 'package.package'
 local fsu     = require 'fs-utility'
