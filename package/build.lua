0b52593841f0132e38e88a8470511718bf82a1fc
最萌小汐 <sumneko@hotmail.com>
2022-11-10 12:01:58 +0800

    more metadata

--- package/build.lua
+++ package/build.lua
@@ -10,6 +10,11 @@ package.__metadata = {
     id = "3a15b5a7-be12-47e3-8445-88ee3eabc8b2",
     publisherDisplayName = "sumneko",
     publisherId = "fb626675-24cf-4881-8c13-b465f29bec2f",
+    targetPlatform = "win32-x64",
+    isApplicationScoped = false,
+    updated = true,
+    isPreReleaseVersion = false,
+    preRelease = false,
 }
 
 local encodeOption = {
