18080896ad28bbb71ee5a1f89af55cdbc6890371
carsakiller <carsakiller@gmail.com>
2023-01-20 01:01:25 -0500

    fix: add fallback category for logging

--- client/src/addon_manager/services/logging.service.ts
+++ client/src/addon_manager/services/logging.service.ts
@@ -25,7 +25,10 @@ export const logger = winston.createLogger({
         winston.format.errors({ stack: true }),
         winston.format.printf((message) => {
             const level = padText(message.level, 9);
-            const category = padText(message.defaultMeta.category, 18);
+            const category = padText(
+                message?.defaultMeta?.category ?? "GENERAL",
+                18
+            );
             if (typeof message.message === "object")
                 return `[${
                     message.timestamp
