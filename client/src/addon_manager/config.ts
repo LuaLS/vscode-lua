d3710beb9c7a9da2b3e86b1dee2801c9da2d0b4b
carsakiller <carsakiller@gmail.com>
2023-02-03 11:30:06 -0500

    chore: update github urls

--- client/src/addon_manager/config.ts
+++ client/src/addon_manager/config.ts
@@ -2,12 +2,12 @@
 export const DEVELOPMENT_IFRAME_URL = "http://127.0.0.1:5173";
 
 // GitHub Repository Info
-export const REPOSITORY_PATH = "https://github.com/carsakiller/LLS-Addons.git";
+export const REPOSITORY_PATH = "https://github.com/LuaLS/LLS-Addons.git";
 export const REPOSITORY_OWNER = "carsakiller";
 export const REPOSITORY_NAME = "LLS-Addons";
 export const REPOSITORY_DEFAULT_BRANCH = "main";
 export const REPOSITORY_ISSUES_URL =
-    "https://github.com/carsakiller/vscode-lua-webvue/issues/new?template=bug_report.yml";
+    "https://github.com/LuaLS/vscode-lua-webvue/issues/new?template=bug_report.yml";
 export const ADDONS_DIRECTORY = "addons";
 
 // settings.json file info
