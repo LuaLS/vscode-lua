4ed37ae2fe74cff9bf36a1b6e9326c6a6cc6a3c4
carsakiller <carsakiller@gmail.com>
2023-03-02 20:44:26 -0500

    fix: direct issue report toast to vscode-lua

--- client/src/addon_manager/config.ts
+++ client/src/addon_manager/config.ts
@@ -7,7 +7,7 @@ export const REPOSITORY_OWNER = "carsakiller";
 export const REPOSITORY_NAME = "LLS-Addons";
 export const REPOSITORY_DEFAULT_BRANCH = "main";
 export const REPOSITORY_ISSUES_URL =
-    "https://github.com/LuaLS/vscode-lua-webvue/issues/new?template=bug_report.yml";
+    "https://github.com/LuaLS/vscode-lua/issues/new?template=bug_report.yml";
 export const ADDONS_DIRECTORY = "addons";
 export const GIT_DOWNLOAD_URL = "https://git-scm.com/downloads";
 
