5fa0fbb154298035fb78b0d142ac8feaf68fc171
最萌小汐 <sumneko@hotmail.com>
2025-02-06 15:16:50 +0800

    3.13.6

--- package/build.lua
+++ package/build.lua
@@ -1,6 +1,6 @@
 local json = require 'json-beautify'
 
-local VERSION = "3.13.5"
+local VERSION = "3.13.6"
 
 local fsu     = require 'fs-utility'
 local package = json.decode(fsu.loadFile(ROOT / 'package.json'))
